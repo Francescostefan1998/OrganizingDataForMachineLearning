4ac7f5fc7f1f3b85a07d6e866dd154ce165fa851
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-01-09 21:29:52 +0100

    filling all the nan value with the mean using simpleimputer and pandas

--- identifyingMissingValues.py
+++ identifyingMissingValues.py
@@ -35,3 +35,16 @@ print(df.dropna(thresh=4))
 
 print("Only drop rows where NaN appear in specific columns (here is 2)")
 print(df.dropna(subset=[2]))
+
+
+print("------------------------------------------")
+
+from sklearn.impute import SimpleImputer
+import numpy as np
+imr = SimpleImputer(missing_values=np.nan, strategy='mean')
+imr = imr.fit(df.values)
+imputed_data = imr.transform(df.values)
+print(imputed_data)
+
+print("------------------------------------------")
+print(df.fillna(df.mean()))
