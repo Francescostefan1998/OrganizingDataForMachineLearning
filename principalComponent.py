4f1658e57a8eefff647a96fc7e57355c4c72918a
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-01-18 20:45:08 +0100

    showing the principal component in a chart

--- principalComponent.py
+++ principalComponent.py
@@ -5,10 +5,34 @@ df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases
 # print(df_wine)
 
 from sklearn.model_selection import train_test_split
-X, y = df_wine.iloc[:, 1].values, df_wine.iloc[:, 0].values
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y, random_state=0)
 # standardize the features
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train_std = sc.fit_transform(X_train)
 X_test_std = sc.transform(X_test)
+
+import numpy as np
+cov_mat = np.cov(X_train_std.T)
+print("cov_mat: ", cov_mat)
+eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
+print('\nEigenValues \n', eigen_vals)
+
+tot = sum(eigen_vals)
+var_exp = [(i/tot) for i in 
+           sorted(eigen_vals, reverse=True)]
+cum_var_exp = np.cumsum(var_exp)
+import matplotlib.pyplot as plt
+plt.bar(range(1,14), var_exp, align='center',
+        label='Individual explained variance')
+plt.step(range(1,14), cum_var_exp, where='mid', 
+         label='Cumulative explained variance')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal component index')
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
+
+
+
