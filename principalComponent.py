292f69f1bb32d41023c5c70d2bdbae788f046756
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-01-25 13:00:26 +0100

    using logistic regression to train the model based on the data gotte from the PCA

--- principalComponent.py
+++ principalComponent.py
@@ -54,13 +54,84 @@ print(X_train_pca)
 
 colors = ['r', 'b', 'g']
 markers = ['o', 's', '^']
-for l, c, m in zip(np.unique(y_train), colors, markers):
-    plt.scatter(X_train_pca[y_train==l, 0],
-                X_train_pca[y_train==l, 1],
-                c=c, label=f'Class {l}', marker=m)
+# for l, c, m in zip(np.unique(y_train), colors, markers):
+#     plt.scatter(X_train_pca[y_train==l, 0],
+#                 X_train_pca[y_train==l, 1],
+#                 c=c, label=f'Class {l}', marker=m)
     
+# plt.xlabel('PC 1')
+# plt.ylabel('PC 2')
+# plt.legend(loc='lower left')
+# plt.tight_layout()
+# plt.show()
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, test_idx=None,
+                          resolution=0.02):
+    # setup marker generator and color map
+    markers = ('o', 's', '^','v','<')
+    colors  = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    # plot the decision surface 
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class examples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y== cl, 0],
+                    y = X[y == cl, 1],
+                    alpha = 0.8,
+                    c=colors[idx],
+                    marker = markers[idx],
+                    label = f'Class {cl}',
+                    edgecolor = 'black')
+        
+    # highlight test examples
+#     if test_idx:
+#         # plot all examples
+#         X_test, y_test = X[test_idx, :], y[test_idx]
+
+#         plt.scatter(X_test[:, 0], X_test[:, 1],
+#                     c='none', edgecolor='black', alpha=1.0,
+#                     linewidth=1, marker='o',
+#                     s=100, label='Test set')
+        
+from sklearn.linear_model import LogisticRegression 
+from sklearn.decomposition import PCA
+# initializing the PCA transformer and logistic regression estimator
+pca = PCA(n_components=2)
+lr = LogisticRegression(multi_class='ovr',
+                        random_state=1,
+                        solver='lbfgs')
+# dimensionality reduction
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.fit_transform(X_test_std)
+# fitting the logistic regression model on the reduced dataset:
+lr.fit(X_train_pca, y_train)
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
 plt.xlabel('PC 1')
 plt.ylabel('PC 2')
 plt.legend(loc='lower left')
 plt.tight_layout()
 plt.show()
+
+# test 
+plot_decision_regions(X_test_pca, y_test, classifier=lr)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+
+pca = PCA(n_components=None)
+X_train_pca = pca.fit_transform(X_train_std)
+pca.explained_variance_ratio_
+print(pca.explained_variance_ratio_)
