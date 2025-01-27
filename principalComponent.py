7c8d8eca0f30287cfa0660a3ffeeea4fbd4c2868
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-01-27 21:45:39 +0100

    start building the scatter matrix

--- principalComponent.py
+++ principalComponent.py
@@ -167,4 +167,19 @@ for label in range(1, 4):
     mean_vecs.append(np.mean(
         X_train_std[y_train==label], axis=0))
     print(f'MV {label}: {mean_vecs[label - 1]}\n')
-    
+
+d = 13 # number of features
+S_W = np.zeros((d, d))
+for label, mv in zip(range(1, 4), mean_vecs):
+    class_scatter = np.zeros((d, d))
+    for row in X_train_std[y_train == label]:
+        row, mv = row.reshape(d, 1), mv.reshape(d, 1)
+        class_scatter += (row -mv).dot((row - mv).T)
+    S_W += class_scatter
+
+print('Within-class scatter matrix: '
+    f'{S_W.shape[0]}x{S_W.shape[1]}')
+
+print('Class labels distribution:',
+        np.bincount(y_train)[1:])
+        
