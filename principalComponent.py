9bdb139a8593fe95d8adc0066f12e15850bfafae
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-01-27 22:26:25 +0100

    selecting linear discriminant

--- principalComponent.py
+++ principalComponent.py
@@ -204,3 +204,16 @@ for i, mean_vec in enumerate(mean_vecs):
         (mean_vec - mean_overall).T)
 print('Between-class scatter matrix: '
         f'{S_B.shape[0]}x{S_B.shape[1]}')
+
+# select the linear discriminant for the neew feature subspace 
+eigen_vals, eigen_vecs = \
+    np.linalg.eig(np.linalg.inv(S_W).dot(S_B))
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i])
+                for i in range(len(eigen_vals))]
+eigen_pairs = sorted(eigen_pairs,
+            key=lambda k: k[0], reverse=True)
+
+print('Eigenvalues in descending order:\n')
+for eigen_val in eigen_pairs:
+    print(eigen_val[0])
+
